8040412831848077746496eb857bd99e521e98ba
melvbouv <melvin.bouvier@isen-ouest.yncrea.fr>
2025-05-04 17:44:41 +0200

    feat(api): ajout route pour charger les fichiers depuis R2

---
+++ functions/api/file/[key].js
@@ -0,0 +1,21 @@
+// /functions/api/file/[key].js
+export const onRequestGet = async ({ env, params }) => {
+    const { BUCKET } = env;
+    const key = params.key;
+  
+    try {
+      const file = await BUCKET.get(key);
+      if (!file) {
+        return new Response("Not Found", { status: 404 });
+      }
+  
+      const headers = new Headers();
+      file.writeHttpMetadata(headers);
+      headers.set("etag", file.etag);
+  
+      return new Response(file.body, { headers });
+    } catch (e) {
+      console.error("Erreur chargement fichier R2 :", e);
+      return new Response("Internal Server Error", { status: 500 });
+    }
+  };
